2d20340cecc120eb794a794ee339b0da370079f3
rileyjson <142921318+rileyjson@users.noreply.github.com>
2024-09-03 17:38:05 -0400

    added new gui design

--- src/GUI/CalculatorFrame.java
+++ src/GUI/CalculatorFrame.java
@@ -1,23 +1,29 @@
 package GUI;
 
 import Calculator.GUITest;
-
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Objects;
 
 public class CalculatorFrame {
+
+    private String op = "";
+    private String firstNumber = "";
+    private String secondNumber = "";
     public CalculatorFrame(){
         JFrame frame = new JFrame();
         JPanel mainPanel = new JPanel();
-        mainPanel.setBorder(BorderFactory.createEmptyBorder(100,100,100,100));
-        mainPanel.setLayout(new GridLayout(1,4));
+        JTextField textField = new JTextField(16);
+        JButton[] numberButtons = new JButton[9];
 
-        NumberButton zero = new NumberButton(0);
-        NumberButton one = new NumberButton(1);
-        NumberButton two = new NumberButton(2);
-        NumberButton three = new NumberButton(3);
+        JButton zero = new JButton(String.valueOf(0));
+        JButton one = new JButton(String.valueOf(1));
+        JButton two = new JButton(String.valueOf(2));
+        JButton three = new JButton(String.valueOf(3));
+
+        mainPanel.add(textField);
 
         mainPanel.add(zero);
         mainPanel.add(one);
@@ -25,44 +31,10 @@ public class CalculatorFrame {
         mainPanel.add(three);
 
         JButton addButton = new JButton("+");
-        addButton.addActionListener(new ActionListener() {
-            /*While these _can_ be replaced with Lambda,
-              I'm going to forgoe that until we figure out how
-              we get the number AFTER add is pressed
-             */
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                GUITest.print("add");
-            }
-        });
         JButton subtractButton = new JButton("-");
-        subtractButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                GUITest.print("sub");
-            }
-        });
         JButton multiplyButton = new JButton("*");
-        multiplyButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                GUITest.print("times");
-            }
-        });
         JButton divideButton = new JButton("/");
-        divideButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                GUITest.print("div");
-            }
-        });
         JButton equalsButton = new JButton("=");
-        equalsButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                GUITest.print("equals");
-            }
-        });
 
         mainPanel.add(addButton);
         mainPanel.add(subtractButton);
@@ -70,10 +42,76 @@ public class CalculatorFrame {
         mainPanel.add(multiplyButton);
         mainPanel.add(equalsButton);
 
-        frame.add(mainPanel, BorderLayout.CENTER);
+        numberButtons[0] = zero;
+        numberButtons[1] = one;
+        numberButtons[2] = two;
+        numberButtons[3] = three;
+
+        numberButtons[4] = addButton;
+        numberButtons[5] = subtractButton;
+        numberButtons[6] = multiplyButton;
+        numberButtons[7] = divideButton;
+        numberButtons[8] = equalsButton;
+
+
+        for (int i = 0; i < numberButtons.length; i++) {
+            numberButtons[i].addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+
+                    String command = e.getActionCommand();
+
+                    if (Objects.equals(command, "0") || Objects.equals(command, "1") || Objects.equals(command, "2") || Objects.equals(command, "3")) {
+
+                        if (op.isEmpty()) {
+                            secondNumber = textField.getText() + command;
+                            textField.setText(secondNumber); //gets second # input
+
+                        }
+                        else {
+                            firstNumber = textField.getText() + command;
+                            textField.setText(firstNumber); //gets first # input
+                        }
+
+                        System.out.println("First Number: " + firstNumber);
+                        System.out.println("Second Number: " + secondNumber);
+
+                    }
+                    else  {
+                        if (command != "=") {
+                            op = command;
+                            System.out.println(op);
+                            textField.setText("");
+                        }
+                    }
+
+                    if (command == "=") {
+                        System.out.println(op);
+                        Calculate(firstNumber ,secondNumber, op);
+                    }
+                }});
+        }
+
+        frame.add(mainPanel);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setTitle("Quaternary Calculator");
         frame.pack();
+        frame.setSize(200, 200);
         frame.setVisible(true);
+    }
+
+    public void Calculate(String val1, String val2, String operator) {
+
+        if (Objects.equals(operator, "+")) {
+            int intVal1 = Integer.parseInt(val1);
+            int intVal2 = Integer.parseInt(val2);
+            System.out.println("Sum: " + (intVal1 + intVal2));
+        }
+        else {
+            System.out.println("WRONG");
+            System.out.println(val1);
+            System.out.println(val2);
+            System.out.println(operator);
+        }
     }
 }
