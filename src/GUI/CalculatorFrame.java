594c8e71acedba18297ffd41491675892a7304ab
Brooke Wright <brookerwright2@gmail.com>
2024-09-04 10:26:23 -0400

    Change the GUI layout to look organized

--- src/GUI/CalculatorFrame.java
+++ src/GUI/CalculatorFrame.java
@@ -5,8 +5,7 @@ import Calculator.Operands;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+
 
 public class CalculatorFrame {
     private String firstNumber = "0";
@@ -16,8 +15,8 @@ public class CalculatorFrame {
     public CalculatorFrame(){
         JFrame frame = new JFrame();
         JPanel mainPanel = new JPanel();
-        mainPanel.setBorder(BorderFactory.createEmptyBorder(100,100,100,100));
-        mainPanel.setLayout(new GridLayout(1,4));
+        mainPanel.setBorder(BorderFactory.createEmptyBorder(10,50,10,50));
+        mainPanel.setLayout(new GridLayout(2,1));
 
         outputLabel = new JLabel();
         outputLabel.setBorder(BorderFactory.createEtchedBorder());
@@ -29,18 +28,23 @@ public class CalculatorFrame {
         NumberButton three = new NumberButton(3);
         NumberButton[] numberButtons = {zero,one,two,three};
 
+        JPanel buttonPanel = new JPanel();
+        buttonPanel.setLayout(new GridLayout(0,4));
+
         for( NumberButton button : numberButtons){
             button.addActionListener(e -> appendToOutputLabel(button.getValue()));
-            mainPanel.add(button);
+            buttonPanel.add(button);
         }
 
         OperandButton addButton = new OperandButton(Operands.ADD, "+");
 
-        mainPanel.add(addButton);
+        buttonPanel.add(addButton);
 //        mainPanel.add(subtractButton);
 //        mainPanel.add(divideButton);
 //        mainPanel.add(multiplyButton);
 //        mainPanel.add(equalsButton);
+
+        mainPanel.add(buttonPanel);
 
         frame.add(mainPanel, BorderLayout.CENTER);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
