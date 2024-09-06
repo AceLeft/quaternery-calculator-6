171d436f7a1942a7b3cb89787130fe686d8ffe64
Hunter Johns <hunter.johns@bsu.edu>
2024-09-06 17:21:17 -0400

    Refactor Calculate.java and implement unit testing for methods found within

--- src/Calculator/Calculate.java
+++ src/Calculator/Calculate.java
@@ -6,45 +6,77 @@ public class Calculate {
 
     public void calculation(String val1, String val2, Operands operator, JLabel outputLabel) {
 
-        int num1 = Integer.parseInt(val1, 4); //converts from base 4 to base 10
-        int num2 = Integer.parseInt(val2, 4); //converts from base 4 to base 10
+        int num1 = base4ToBase10(val1); //converts from base 4 to base 10
+        int num2 = base4ToBase10(val2); //converts from base 4 to base 10
 
         switch (operator) {
             case ADD:
-                int sum = num1 + num2;
-                String convertedSum = Integer.toString(sum, 4); //base 10 back to 4
+                int sum = addition(num1, num2);
+                String convertedSum = base10ToBase4(sum); //base 10 back to 4
                 System.out.println(convertedSum);
                 outputLabel.setText(convertedSum);
                 break;
             case SUBTRACT:
-                int difference = num1 - num2;
-                String convertedDifference = Integer.toString(difference, 4);
+                int difference = subtraction(num1, num2);
+                String convertedDifference = base10ToBase4(difference);
                 outputLabel.setText(convertedDifference);
                 break;
             case MULTIPLY:
-                int product = num1 * num2;
-                String convertedProduct = Integer.toString(product, 4);
+                int product = multiplication(num1, num2);
+                String convertedProduct = base10ToBase4(product);
                 outputLabel.setText(convertedProduct);
                 break;
             case DIVIDE:
-                int quotient = num1 / num2;
-                String convertedQuotient = Integer.toString(quotient, 4);
+                int quotient = division(num1, num2);
+                String convertedQuotient = base10ToBase4(quotient);
                 outputLabel.setText(convertedQuotient);
                 break;
             case SQUARE:
-                int square = num1*num1;
-                String convertedSquare = Integer.toString(square, 4);
+                int square = multiplication(num1,num1);
+                String convertedSquare = base10ToBase4(square);
                 outputLabel.setText(convertedSquare);
                 break;
             case SQUAREROOT:
-                int squareRoot = (int) Math.sqrt(num1);
-                String convertedSquareRoot = Integer.toString(squareRoot, 4);
+                int squareRoot = squareRoot(num1);
+                String convertedSquareRoot = base10ToBase4(squareRoot);
                 outputLabel.setText(convertedSquareRoot);
                 break;
 
         }
 
-
     }
 
+
+    public int base4ToBase10(String value){
+        return Integer.parseInt(value, 4);
+    }
+
+    public String base10ToBase4(int value){
+        return Integer.toString(value, 4);
+    }
+
+
+    public int addition (int val1, int val2){
+        return (val1 + val2);
+    }
+
+
+    public int subtraction(int val1, int val2){
+        return (val1 - val2);
+    }
+
+
+    public int multiplication(int val1, int val2){
+        return (val1 * val2);
+    }
+
+
+    public int division(int val1, int val2){
+        return (val1/val2);
+    }
+
+
+    public int squareRoot(int value){
+        return (int) Math.sqrt(value);
+    }
 }
--- src/test/CalculateTest.java
+++ src/test/CalculateTest.java
@@ -1,7 +1,56 @@
 package test;
 
-//import Calculator.Calculate;
-//import org.junit.jupiter.api.Test;
-//
-//public class CalculateTest {
-//}
+import Calculator.Calculate;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class CalculateTest {
+    @Test
+    public void base4ToBase10(){
+        Calculate calculateTest = new Calculate();
+        int returnedValue = calculateTest.base4ToBase10("11");
+        Assertions.assertEquals(5, returnedValue);
+    }
+
+    @Test
+    public void base10ToBase4(){
+        Calculate calculateTest = new Calculate();
+        String returnedValue = calculateTest.base10ToBase4(11);
+        Assertions.assertEquals("23", returnedValue);
+    }
+
+    @Test
+    public void addition(){
+        Calculate calculateTest = new Calculate();
+        int returnedValue = calculateTest.addition(1,5);
+        Assertions.assertEquals(6, returnedValue);
+    }
+
+    @Test
+    public void subtraction(){
+        Calculate calculateTest = new Calculate();
+        int returnedValue = calculateTest.subtraction(5,1);
+        Assertions.assertEquals(4, returnedValue);
+    }
+
+    @Test
+    public void multiplication(){
+        Calculate calculateTest = new Calculate();
+        int returnedValue = calculateTest.multiplication(2,5);
+        Assertions.assertEquals(10, returnedValue);
+    }
+
+    @Test
+    public void division(){
+        Calculate calculateTest = new Calculate();
+        int returnedValue = calculateTest.division(10,5);
+        Assertions.assertEquals(2, returnedValue);
+    }
+
+    @Test
+    public void squareRoot(){
+        Calculate calculateTest = new Calculate();
+        int returnedValue = calculateTest.squareRoot(25);
+        Assertions.assertEquals(5, returnedValue);
+    }
+}
